b503d0235b1cf014e36e65e9421881ea213b1b92
Romain Scohy <romain.scohy@outlook.com>
2015-11-21 18:19:33 +0100

    Pull du 21 nov
    Début TD2 avec commandes de bases, mais j'y arrive pas

---
+++ TD2_13_11/src/Stock.java
@@ -0,0 +1,53 @@
+
+public class Stock {
+
+	// Déclaration constantes
+	static int TAILLE_TAB=5;
+	
+	
+	public static void main(String[] args) {
+		// ---
+		// Déclarer Tableaux
+		// ---
+		char tabchar[];
+		double tabdouble[];
+		
+		
+		// ---
+		// Allouer les tableaux
+		// ---
+		tabchar = new char[TAILLE_TAB];
+		//int effectiveTabchar = 0;
+		tabdouble = new double[TAILLE_TAB];
+		//int effectiveTabdouble = 0;
+		
+		// ---
+		// Mettre des valeurs dans les cases
+		// ---
+		tabchar[0]='A';
+		tabchar[1]='B';
+		tabchar[2]='C';
+		
+		tabdouble[0]=10.5;
+		tabdouble[1]=2.5;
+		tabdouble[2]=21.5;
+		final int NB_ARTICLES;
+		NB_ARTICLES=3;
+		// ---
+		// Parcours des 2 tableaux avec 2 boucles foreach
+		// ---
+		for (double i : tabdouble){
+			System.out.println(tabchar[i]);
+			System.out.println(tabdouble[i]);
+		}
+		
+		// ---
+		// Parcours des 2 tableaux en parallèle avec while + bool
+		// ---
+		
+		
+		
+
+	}
+
+}
